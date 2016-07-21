9a2e19d5ea556b3064561f70ad6dab9f83a65cf3
Pepe Barbe <pepe@radiasoft.net>
2016-07-21 16:11:59 -0500

    Missing package for travis

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -4,7 +4,7 @@ set -x -e -u -o pipefail
 
 # http://linuxsimba.com/vagrant-libvirt-install
 apt-get -qq update 
-apt-get -y install jq curl qemu-kvm libvirt-bin libvirt-dev htop tmux libxslt-dev libxml2-dev libvirt-dev zlib1g-dev ruby-dev  qemu-utils libvirt-dev
+apt-get -y install jq curl qemu-kvm libvirt-bin libvirt-dev htop tmux libxslt-dev libxml2-dev libvirt-dev zlib1g-dev ruby-dev  qemu-utils libvirt-dev realpath
 # https://github.com/mitchellh/vagrant-installers/issues/12
 #LATEST_VAGRANT=$(curl -s https://releases.hashicorp.com/vagrant/index.json | jq --raw-output '.versions | to_entries | max_by(.key) | .value.builds | .[] | select(.arch=="x86_64") | select(.os=="debian") | .url ')
 # https://github.com/mitchellh/vagrant/issues/7610
