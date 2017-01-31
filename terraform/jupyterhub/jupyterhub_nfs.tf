6fbefd72d77a9a30f781735c35ce7b0c0100e459
Pepe Barbe <pepe@radiasoft.net>
2017-01-31 17:18:39 -0600

    Initial commit of dummy Nginx container within NFS #12

--- terraform/jupyterhub/jupyterhub_nfs.tf
+++ terraform/jupyterhub/jupyterhub_nfs.tf
@@ -11,6 +11,30 @@ resource "aws_security_group" "jupyterhub_nfs" {
     }
     
     ingress {
+        from_port = 80
+        to_port   = 80
+        protocol  = "tcp"
+
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 8080
+        to_port   = 8080
+        protocol  = "tcp"
+
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    
+    ingress {
+        from_port = 443
+        to_port   = 443
+        protocol  = "tcp"
+
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    
+    ingress {
         from_port = 8
         to_port   = 0
         protocol  = "icmp"
