f440b2f3f60409f7ca4d4d5d010229f2a9a78bcb
Pepe Barbe <pepe@radiasoft.net>
2017-02-20 23:44:36 -0600

    Create ssh config file with the right parameters to connect to workers

--- filter_plugins/docker_helpers.py
+++ filter_plugins/docker_helpers.py
@@ -3,8 +3,12 @@ import socket
 def docker_add_hosts(domains):
     return [ '{0}:{1}'.format(d, socket.gethostbyname(d)) for d in domains ]
 
+def docker_get_ip(domain):
+    return socket.gethostbyname(domain)
+
 class FilterModule(object):
     def filters(self):
         return {
-             'docker_add_hosts': docker_add_hosts,
+            'docker_add_hosts': docker_add_hosts,
+            'docker_get_ip': docker_get_ip,
         }
